51d9d277699c4bee855ac48614cd9a6de9143c34
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2018-04-12 23:32:39 -0500

    Added a dev env.

---
+++ dev-resources/src/hxgm30/mush/dev.clj
@@ -0,0 +1,105 @@
+(ns hxgm30.mush.dev
+  (:require
+    [clojure.java.io :as io]
+    [clojure.pprint :refer [pprint]]
+    [clojure.string :as string]
+    [clojure.tools.namespace.repl :as repl]
+    [clojusc.dev.system.core :as system-api]
+    [clojusc.twig :as logger]
+    [com.stuartsierra.component :as component]
+    [hxgm30.graphdb.plugin.backend :as graphdb-backend]
+    [hxgm30.mush.components.config :as config]
+    [hxgm30.mush.components.core]
+    [trifl.java :refer [show-methods]]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Initial Setup & Utility Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(logger/set-level! '[hxgm30] :debug)
+
+(def ^:dynamic *mgr* nil)
+
+(defn banner
+  []
+  (println (slurp (io/resource "text/banner.txt")))
+  :ok)
+
+(defn mgr-arg
+  []
+  (if *mgr*
+    *mgr*
+    (throw (new Exception
+                (str "A state manager is not defined; "
+                     "have you run (startup)?")))))
+
+(defn system-arg
+  []
+  (if-let [state (:state *mgr*)]
+    (system-api/get-system state)
+    (throw (new Exception
+                (str "System data structure is not defined; "
+                     "have you run (startup)?")))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Data Support   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn status
+  []
+  (system-api/get-status (:state (mgr-arg))))
+
+(defn system
+  []
+  (system-api/get-system (:state (mgr-arg))))
+
+(defn backend
+  []
+  (config/db-plugin (system)))
+
+(defn graphdb-conn
+  []
+  (graphdb-backend/get-conn (backend) (system)))
+
+(defn graphdb-factory
+  []
+  (graphdb-backend/get-factory (backend) (system)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   State Management   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn startup
+  []
+  (alter-var-root #'*mgr* (constantly (system-api/create-state-manager)))
+  (system-api/set-system-ns (:state *mgr*) "hxgm30.mush.components.core")
+  (system-api/startup *mgr*)
+  (status))
+
+(defn shutdown
+  []
+  (when *mgr*
+    (let [result (system-api/shutdown (mgr-arg))]
+      (alter-var-root #'*mgr* (constantly nil))
+      result)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Reloading Management   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn reset
+  []
+  (shutdown)
+  (repl/refresh :after 'hxgm30.mush.dev/startup))
+
+(def refresh #'repl/refresh)
+
+(comment
+  (startup)
+  (graphdb-backend/db-call (backend) (system) 'vertices)
+  (def cave (db/add-vertex g {:type :room :name "A cave" :description "You are in a dark cave."}))
+  (def tunnel (db/add-vertex g {:type :room
+                                :name "A tunnel"
+                                :description "You are in a long, dark tunnel."}))
+  )
